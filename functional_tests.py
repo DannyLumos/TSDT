5fe6e707cba849d660677e1bdabe5f8203b94367
Liu Yida <2766383652@qq.com>
2024-04-11 15:43:39 +0800

    message 9:After refactoring the functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) :
         self.browser.quit()
     
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn(row_text,[row.text for row in rows])
+    
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # 张三听说有一个在线代办事项的应用
@@ -39,17 +44,15 @@ class NewVisitorTest(unittest.TestCase):
         # 待办事项表格中显示了“1:Buy flowers”
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy flowers')
 
         inputbox = self.browser.find_element(By.ID,'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-
-        table = self.browser.find_element(By.ID,'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn('1: Buy flowers', [row.text for row in rows])
-        self.assertIn('2:Give a gift to Lisi',[row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy flowers')
+        self.check_for_row_in_list_table('2: Give a gift to Lisi')
 
         # 页面中又显示了一个文本输入框，可以输入其他待办事项
         # 他输入了“Send a gift to Lisi”
