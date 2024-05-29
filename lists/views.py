871473a3251663ca358ed4676f61191f3d68f7b1
Liu Yida <2766383652@qq.com>
2024-05-29 20:15:15 +0800

    message 22 ：use Bootstrap to improve layout

--- lists/views.py
+++ lists/views.py
@@ -7,7 +7,7 @@ def home_page(request):
 
 def view_list(request,list_id):
     list_user = List.objects.get(id=list_id)
-    return render(request,'list.html',{'list':list_user})
+    return render(request,'list.html',{'list': list_user})
 
 def new_list(request):
     list_user = List.objects.create()
